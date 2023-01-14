8d355e626992a0c9158dd2baff58543d3db6fb5f
Artem Malafeev <ma.evgenyevich@gmail.com>
2023-01-14 20:13:03 +0300

    change data

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -8,54 +8,42 @@
               <li class="calculator__item">f</li>
             </ul>
           </div>
-          <p v-if="state === 'processed'" class="calculator__result">{{ previous }}</p>
-          <p v-else class="calculator__result">{{ current }}</p>
+          <p class="calculator__result">{{ previous }}</p>
+          <p class="calculator__result">{{ current }}</p>
         </div>
       </div>
       <div class="calculator__menu">
-        <!-- update -->
-        <button :style="{ 'grid-area': 'zero' }" class="calculator__button" @click="updateCurrent">0</button>
-        <button :style="{ 'grid-area': 'one' }" class="calculator__button" @click="updateCurrent">1</button>
-        <button :style="{ 'grid-area': 'two' }" class="calculator__button" @click="updateCurrent">2</button>
-        <button :style="{ 'grid-area': 'three' }" class="calculator__button" @click="updateCurrent">3</button>
-        <button :style="{ 'grid-area': 'four' }" class="calculator__button" @click="updateCurrent">4</button>
-        <button :style="{ 'grid-area': 'five' }" class="calculator__button" @click="updateCurrent">5</button>
-        <button :style="{ 'grid-area': 'six' }" class="calculator__button" @click="updateCurrent">6</button>
-        <button :style="{ 'grid-area': 'seven' }" class="calculator__button" @click="updateCurrent">7</button>
-        <button :style="{ 'grid-area': 'eight' }" class="calculator__button" @click="updateCurrent">8</button>
-        <button :style="{ 'grid-area': 'nine' }" class="calculator__button" @click="updateCurrent">9</button>
-        <button :style="{ 'grid-area': 'comma' }" class="calculator__button" @click="updateCurrent">.</button>
-        <!-- action -->
-        <button :style="{ 'grid-area': 'del' }" class="calculator__button calculator__button--action" @click="action('del')">AC</button>
-        <button :style="{ 'grid-area': 'change' }" class="calculator__button calculator__button--action" @click="action('+/-')">+/-</button>
-        <button :style="{ 'grid-area': 'percent' }" class="calculator__button calculator__button--action" @click="action('%')">%</button>
-        <button :style="{ 'grid-area': 'divide' }" class="calculator__button calculator__button--action" @click="action('/')">/</button>
-        <button :style="{ 'grid-area': 'multiply' }" class="calculator__button calculator__button--action" @click="action('X')">X</button>
-        <button :style="{ 'grid-area': 'minus' }" class="calculator__button calculator__button--action" @click="action('-')">-</button>
-        <button :style="{ 'grid-area': 'plus' }" class="calculator__button calculator__button--action" @click="action('+')">+</button>
-        <button :style="{ 'grid-area': 'equals' }" class="calculator__button calculator__button--action" @click="result">=</button>
+        <calculator-buttons :buttons="buttons" @click="handler" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  import { actions } from '../data.js';
+  import CalculatorButtons from './CalculatorButtons.vue';
+  import { buttons, actions } from '../data.js';
 
   export default {
     name: 'Calculator',
+
+    components: { CalculatorButtons },
 
     data() {
       return {
         previous: 0,
         current: 0,
-        operator: null,
+        previousOperation: null,
+
         state: 'start',
-        actions: actions,
+        buttons: buttons,
       };
     },
 
     methods: {
+      handler(event) {
+        console.log(event.target);
+      },
+
       updateCurrent({ target }) {
         const separator = '';
         const inputValue = target.innerHTML;
@@ -69,30 +57,31 @@
       action(operator) {
         if (this.state === 'start') {
           this.previous = this.current;
-          this.state = 'processing';
-        }
-
-        this.operator = operator;
-        const { action, type } = actions[operator];
-
-        if (type === 'unary') {
-          this.result();
-        }
-
-        this.current = 0;
-      },
-
-      result() {
-        if (this.operator !== null) {
-          this.state = 'processed';
-          const { action, type } = this.actions[this.operator];
-          this.previous = (type === 'binary') ? action(this.previous, this.current) : action(this.previous);
-          this.operator = null;
           this.current = 0;
-        } else {
-          alert('Выберите оператор!')
         }
-      }
+
+        this.state = 'processing';
+        const { action: fn, type } = this.actions[operator];
+
+        switch (type) {
+          case 'unary': {
+            this.previous = fn(this.previous);
+            this.current = 0;
+            break;
+          }
+
+          case 'binary': {
+            this.previousOperation = fn;
+            break;
+          }
+
+          case 'finall': {
+            this.previous = fn(this.previousOperation)(this.previous, this.current);
+            this.current = 0;
+            break;
+          }
+        }
+      },
     }
   }
 </script>
---
+++ src/components/CalculatorButton.vue
@@ -0,0 +1,51 @@
+<template>
+  <button
+    :style="{ 'grid-area': getArea }"
+    class="calculator__button"
+    :class="{ 'calculator__button--action': isAction }"
+    @click="$emit('ar', $event)"
+    >
+      <span>{{ getSpan }}</span>
+  </button>
+</template>
+
+<script>
+  export default {
+    name: 'CalculatorButton',
+
+    emits: ['ar'],
+
+    props: {
+      button: {
+        type: Object,
+        requried: true,
+      },
+
+      area: {
+        type: String,
+        required: true,
+      },
+
+      span: {
+        type: String,
+        required: true,
+      },
+
+      
+    },
+
+    computed: {
+      getArea() {
+        return this.button.area;
+      },
+
+      getSpan() {
+        return this.button.span;
+      },
+
+      isAction() {
+        return this.button.type === 'action';
+      },
+    },
+  }
+</script>
