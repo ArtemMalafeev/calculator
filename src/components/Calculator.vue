019eb2bfdccb928d6b0f5c91c9292b78a85b63be
Artem Malafeev <ma.evgenyevich@gmail.com>
2023-01-24 16:03:47 +0300

    work with base logic and architecture

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,56 +1,162 @@
 <template>
-  <div>
-    <p>Current: {{ this.current }}</p>
-    <p>Previous: {{ this.previous }}</p>
-    <p>Operation: {{ this.operation }}</p>
+  <div class="info">
+    <p>Prev: {{ operands.previous }} {{ typeof operands.previous }}</p>
+    <p>Operand: {{ operands.current }} {{ typeof operands.current }}</p>
+    <p>Action: {{ action }}</p>
+    <p>Operator: {{ operator }}</p>
+    <p>State: {{ state }}</p>
+    <p>History: {{ history }}</p>
+  </div>
 
-    <button v-for="value in $options.values" @click="handleValue(value.value)">{{ value.span }}</button>
-    <button v-for="action in $options.actions" @click="handleAction(action)">{{ action.span }}</button>
-    <button>C</button>
-    <button>=</button>
+  <hr>
+
+  <div class="values">
+    <!-- span - для отображения содержимого -->
+    <!--
+      value - для определения значения кнопки
+      ?? вопрос передавать просто значение в функцию или определять в data и вызывать без передачи параметров
+    -->
+    <button @click="handleAction" data-action-type="value" data-value="0">0</button>
+    <button @click="handleAction" data-action-type="value" data-value="1">1</button>
+    <button @click="handleAction" data-action-type="value" data-value="2">2</button>
+    <button @click="handleAction" data-action-type="value" data-value="3">3</button>
+    <button @click="handleAction" data-action-type="value" data-value="4">4</button>
+    <button @click="handleAction" data-action-type="value" data-value="5">5</button>
+    <button @click="handleAction" data-action-type="value" data-value="6">6</button>
+    <button @click="handleAction" data-action-type="value" data-value="7">7</button>
+    <button @click="handleAction" data-action-type="value" data-value="8">8</button>
+    <button @click="handleAction" data-action-type="value" data-value="9">9</button>
+    <button @click="handleAction" data-action-type="value" data-value=".">.</button>
+  </div>
+
+  <hr>
+
+  <div class="operators">
+    <!-- span - для отображения содержимого -->
+    <button @click="handleAction" data-action-type="operator" data-type="binary" data-operator="add">+</button>
+    <button @click="handleAction" data-action-type="operator" data-type="binary" data-operator="subtract">-</button>
+    <button @click="handleAction" data-action-type="operator" data-type="binary" data-operator="multiply">X</button>
+    <button @click="handleAction" data-action-type="operator" data-type="binary" data-operator="divide">/</button>
+    <button @click="handleAction" data-action-type="operator" data-type="unary" data-operator="sign">+/-</button>
+    <button @click="handleAction" data-action-type="operator" data-type="unary" data-operator="percent">%</button>
+  </div>
+
+  <hr>
+
+  <div class="action">
+    <button>AC</button>
+    <button @click="calculate">=</button>
   </div>
 </template>
 
 <script>
-  import { values, actions } from '../data.js';
+  import { operations } from '../data';
 
   export default {
     name: 'Calculator',
 
-    values,
-    actions,
-
     data() {
       return {
-        previous: 0,
-        current: 0,
-        operation: null,
+        state: 'default',
+        action: null,
+        operator: null,
+        operands: {
+          previous: 0,
+          current: 0,
+        },
+
+        history: [],
       };
     },
 
-    methods: {
-      handleValue(value) {
-        const separator = '';
-        const newCurrent = [this.current, value].join(separator);
-
-        this.current = (value === '.')
-          ? newCurrent
-          : parseFloat(newCurrent);
+    computed: {
+      getCurrentOperand() {
+        return this.operands.current;
       },
 
-      handleAction(action) {
-        const { type, action: fn } = action;
-        this.previous = this.current;
-        this.operation = fn;
+      getPreviousOperand() {
+        return this.operands.previous;
+      },
 
-        if (type === 'unary') {
-          this.calculate();
+      getOperator() {
+        return this.operator;
+      },
+
+      getState() {
+        return this.state;
+      },
+    },
+
+    methods: {
+      handleAction({ target }) {
+        this.action = { ...target.dataset };
+      },
+
+      updateOperand(value) {
+        const separator = '';
+        const number = parseInt([this.getCurrentOperand, value].join(separator));
+        this.operands.current = number;
+      },
+
+      updateOperator(data) {
+        this.operator = { ...data };
+      },
+
+      // calculate() {
+      //   if (this.getState === 'processed' || this.getState === 'default') {
+      //     console.log('Выберите оператор!');
+      //   }
+      //   const { type, operator } = this.operator;
+      //   const operation = operations[operator];
+
+      //   this.operands.previous = operation(this.getPreviousOperand, this.getCurrentOperand);
+      //   this.operator = null;
+      // },
+    },
+
+    watch: {
+      action() {
+        const { actionType } = this.action;
+
+        switch(actionType) {
+          case 'value': {
+            const { value } = this.action;
+            this.updateOperand(value);
+            break;
+          }
+          case 'operator': {
+            const { type, operator } = this.action;
+            this.updateOperator({ type, operator });
+            break;
+          }
+          case 'clear': {
+            // this.reset();
+            break;
+          }
+          case 'calculate': {
+            // this.calculate();
+            break;
+          }
         }
       },
 
-      calculate() {
-        this.previous = this.operation(this.previous);
-      }
-    }
+      // operator: {
+      //   deep: true,
+      //   handler(newOperator) {
+      //     this.state = (!!newOperator) ? 'processing' : 'processed';
+      //   },
+      // },
+
+      // state(newOperator, oldOperator) {
+      //   if (newOperator === 'processing' && oldOperator === 'default') {
+      //     this.operands.previous = this.operands.current;
+      //     this.operands.current = 0;
+      //   }
+
+      //   if (newOperator === 'processed' && oldOperator === 'processing') {
+      //     this.operands.current = 0;
+      //   }
+      // },
+    },
   }
 </script>
--- src/data.js
+++ src/data.js
@@ -1,22 +1,8 @@
-export const values = [
-  { span: "0", value: 0 },
-  { span: "1", value: 1 },
-  { span: "2", value: 2 },
-  { span: "3", value: 3 },
-  { span: "4", value: 4 },
-  { span: "5", value: 5 },
-  { span: "6", value: 6 },
-  { span: "7", value: 7 },
-  { span: "8", value: 8 },
-  { span: "9", value: 9 },
-  { span: ".", value: '.' },
-];
-
-export const actions = [
-  { span: "+", type: 'binary', action: (operand1, operand2) => operand1 + operand2 },
-  { span: "-", type: 'binary', action: (operand1, operand2) => operand1 - operand2 },
-  { span: "/", type: 'binary', action: (operand1, operand2) => operand1 / operand2 },
-  { span: "X", type: 'binary', action: (operand1, operand2) => operand1 * operand2 },
-  { span: "%", type: 'unary', action: (operand) => operand / 100 },
-  { span: "+/-", type: 'unary', action: (operand) => -operand },
-];
+export const operations = {
+  'add': (operand1, operand2) => operand1 + operand2,
+  'subtract': (operand1, operand2) => operand1 - operand2,
+  'multiply': (operand1, operand2) => operand1 * operand2,
+  'divide': (operand1, operand2) => operand1 / operand2,
+  'sign': (operand) => -1 * operand,
+  'percent': (operand) => operand / 100,
+};
