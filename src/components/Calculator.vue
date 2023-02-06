2f220b8cb654d6da4ef51eea14032f20d2938df6
Artem Malafeev <ma.evgenyevich@gmail.com>
2023-02-06 08:26:44 +0300

    delete old code

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -79,23 +79,23 @@
         this.event = { ...data };
       },
 
-      currentOperandHandler(type, value) {
+      operandsHandler(type, value='0') {
         switch(type) {
           case 'update': {
-            this.updateCurrentOperand(value);
+            this.updateOperand(value);
             break;
           }
           case 'assign':
           case 'reset': {
-            this.assignCurrentOperand(value);
+            this.assignOperand(value);
             break;
           }
         }
       },
 
-      updateCurrentOperand(value) {
+      updateOperand(value) {
         if (this.getCurrentOperandLength === 1 && this.getCurrentOperand.at(0) === '0' && value !== '.') {
-          this.assignCurrentOperand(value);
+          this.assignOperand(value);
         } else {
           const separator = '';
           const regularExpression = /^-?0{1}(\.{1}[0-9]*)?$|^-?[1-9]{1}[0-9]*$|^-?[1-9]{1}[0-9]*(\.{1}[0-9]*)?$/;
@@ -108,166 +108,46 @@
         }
       },
 
-      assignCurrentOperand(value) {
+      assignOperand(value) {
         this.calculationData.operands.current = value;
       },
 
-      // -------
-
-      // checkCorrectOperand(value) {
-
+      // operatorHandler(type, func) {
+      //   switch(type) {
+      //     case 'binary': {
+      //       this.updatePreviousOperand();
+      //       this.currentOperandHandler('reset');
+      //       break;
+      //     }
+      //     case 'unary': {
+      //       break;
+      //     }
+      //   }
       // },
-
-      // reg: /[0-9]+/
-
-      // ----
-
-      // updateOperand(value) {
-      //   const separator = '';
-      //   const number = parseFloat([this.getCurrentOperand, value].join(separator));
-      //   this.calculationData.operands.current = number;
-      // },
-
-      updateOperator(data) {
-        this.calculationData.operator = { ...data };
-      },
-
-      updatePreviousOperand(value) {
-        this.calculationData.operands.previous = value;
-      },
-
-      // updateCurrentOperand(value) {
-      //   this.calculationData.operands.current = value;
-      // },
-
-      updateHistoryAction(value) {
-        if (this.isEmptyHistoryAction) {
-          this.historyAction.push(value);
-        } else {
-          const lastAction = this.getHistoryAction.at(-1);
-          console.log(value, lastAction);
-          if (value !== lastAction) {
-            this.historyAction.push(value);
-          }
-        }
-      },
-
-      resetCurrentOperand() {
-        this.calculationData.operands.current = 0;
-      },
-
-      resetPreviousOperand() {
-        this.calculationData.operands.previous = 0;
-      },
-
-      resetOperator() {
-        this.calculationData.operator = null;
-      },
-
-      resetAction() {
-        this.action = null;
-      },
-
-      clear() {
-        if (this.isEmptyHistoryAction) {
-          console.log('Калькулятор очищен');
-        } else {
-          const lastAction = this.getHistoryAction.at(-1);
-          if (lastAction === 'updateOperand') {
-            this.resetCurrentOperand();
-          }
-
-          if (lastAction === 'updateOperator') {
-            this.resetOperator();
-            this.calculationData.operands.current = this.getPreviousOperand;
-            this.resetPreviousOperand();
-          }
-
-          this.historyAction.pop();
-        }
-      },
-
-      calculate() {
-        if (this.getOperator === null) {
-          alert('Выберите оператор!');
-          return;
-        }
-
-        const type = this.getOperator.type;
-        const operator = this.getOperator.operator;
-        const operation = operations[operator];
-
-        if (type === 'unary') {
-          this.calculationData.operands.current = operation(this.getCurrentOperand);
-        }
-
-        if (type === 'binary') {
-          this.calculationData.operands.current = operation(this.getPreviousOperand, this.getCurrentOperand);
-        }
-      },
     },
 
     watch: {
       event() {
         switch(this.getEventType) {
           case 'value': {
-            this.currentOperandHandler('update', this.getEventData.value);
+            this.operandsHandler('update', this.getEventData.value);
             break;
           }
 
           case 'operator': {
-            this.operatorHandler({
-              type: this.getEventData.type,
-              func: this.getEventData.func,
-            });
+            this.operatorHandler(this.getEventData.type, this.getEventData.func);
             break;
           }
 
           case 'clear': {
-            // this.clear();
             break;
           }
 
           case 'calculate': {
-            // this.calculate();
-            // this.resetOperator();
-            // this.resetAction();
-            // this.resetPreviousOperand();
             break;
           }
         }
       },
-
-      // 'calculationData.operator'(newOperator, oldOperator) {
-      //   if (newOperator.type === null) {
-      //     return;
-      //   } else {
-      //     if (newOperator.type === 'binary') {
-      //     this.updatePreviousOperand(this.getCurrentOperand);
-      //     this.resetCurrentOperand();
-          // this.updateHistoryAction('saveOperand');
-          // this.updateHistoryAction('saveOperator');
-          // }}
-        // if (newOperator === null) {
-        //   console.log('-');
-        //   return;
-        // }
-
-        // if (newOperator.type === 'binary') {
-        //   if (oldOperator === null || oldOperator.type === 'unary') {
-        //     console.log('=');
-        //     this.updatePreviousOperand(this.getCurrentOperand);
-        //     this.resetCurrentOperand();
-        //   }
-        // }
-
-        // if (newOperator.type === 'unary') {
-        //   this.calculate();
-        //   this.resetOperator();
-        //   this.resetAction();
-        //   this.resetPreviousOperand();
-        // }
-      // },
     },
   }
 </script>
